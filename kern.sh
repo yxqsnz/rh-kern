d86d58a22645829e9e8c83147eaffecdec45b904
yxqsnz <yxqsnz@gmail.com>
2023-01-19 21:28:49 -0300

    feat: i think this is working

--- kern.sh
+++ kern.sh
@@ -1,15 +1,20 @@
-EFI_PATH="target/x86_64-unknown-uefi/debug/rh-kern.efi"
+export EFI_PATH="target/x86_64-unknown-uefi/debug/rh-kern.efi"
 export CARGO_TARGET_DIR=target
+export VM_PATH="target/virtual-machine"
+export OVMF_PATH="/usr/share/OVMF"
 
-echo "[=] BUILDING"
+echo "--> Building Kernel"
 cargo build --target x86_64-unknown-uefi || exit
 
-echo "[=] EFI DIRECTORY: ${EFI_PATH}"
 
-echo "@= Creating file structure at vm"
-mkdir -p vm/EFI/Boot/
-mv ${EFI_PATH} vm/EFI/Boot/Bootx64.efi
+echo "--> Creating Fake filesystem"
+mkdir -p $VM_PATH/efi/boot
+cp $EFI_PATH $VM_PATH/efi/boot/bootx64.efi
+cp kernel.rhai $VM_PATH
 
-echo "@= Running QEMU"
-qemu-system-x86_64 -drive format=raw,file=fat:rw:vm/ -machine q35 -no-reboot -enable-kvm -bios /usr/share/OVMF/OVMF_CODE.fd
+echo "--> Running Virtual Machine"
+exec qemu-system-x86_64 \
+    -drive if=pflash,format=raw,readonly=on,file=$OVMF_PATH/OVMF_CODE.fd \
+    -drive if=pflash,format=raw,readonly=on,file=$OVMF_PATH/OVMF_VARS.fd \
+    -drive format=raw,file=fat:rw:$VM_PATH
 
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,59 @@
 #![no_std]
 #![feature(abi_efiapi)]
 #![allow(stable_features)]
+extern crate alloc;
 
-use uefi::prelude::*;
+use alloc::{string::String, vec};
+use rhai::{Engine, Scope};
+use uefi::{
+    prelude::*,
+    proto::media::{
+        file::{File, FileAttribute, FileInfo, FileMode},
+        fs::SimpleFileSystem,
+    },
+};
 use uefi_services::println;
+
+fn read_kernel(services: &BootServices) -> String {
+    let handle = services
+        .get_handle_for_protocol::<SimpleFileSystem>()
+        .unwrap();
+    let mut sfs = services
+        .open_protocol_exclusive::<SimpleFileSystem>(handle)
+        .expect("failed to open SimpleFileSystem protocol");
+
+    let mut directory = sfs.open_volume().unwrap();
+    let kernel = directory
+        .open(
+            cstr16!("kernel.rhai"),
+            FileMode::Read,
+            FileAttribute::empty(),
+        )
+        .unwrap();
+    let mut buffer = vec![0; 128];
+    let mut file = kernel.into_regular_file().unwrap();
+    let file_info = file.get_info::<FileInfo>(&mut buffer).unwrap();
+    let file_size = file_info.file_size() as usize;
+    let mut file_buffer = vec![0; file_size];
+
+    file.read(&mut file_buffer).unwrap();
+    let string = String::from_utf8(file_buffer).unwrap();
+
+    string
+}
+
+fn uefi_puts(content: &str) {
+    println!("{content}")
+}
 
 #[entry]
 fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
-    println!("Hello world!");
-    system_table.boot_services().stall(10_000_000);
+    println!("Loading Kernel ...");
+    let mut engine = Engine::new();
+    let mut scope = Scope::new();
+    let kernel = read_kernel(system_table.boot_services());
+    engine.register_fn("uefi_puts", uefi_puts);
+    engine.run_with_scope(&mut scope, &kernel).unwrap();
     Status::SUCCESS
 }
-
